b466626f13c6cfd4a9da601e1ef54783a1cd7342
samar151110 <92369339+samar151110@users.noreply.github.com>
2022-05-30 19:06:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+p1 = "";
+p2 = "";
+
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: "png",
+    png_quality: 90
+});
+
+camera = document.getElementById("Camera");
+Webcam.attach(camera);
+
+function snapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = '<img id="capture_image" src="' + data_uri + '"/>';
+    });
+
+}
+console.log("ml5 version", ml5.version);
+
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/A9oeDX1El/model.json", samar);
+
